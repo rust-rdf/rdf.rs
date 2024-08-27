286ff2d4d13b52c272ba04fecad66f3c734f1524
Arto Bendiken <arto@bendiken.net>
2024-08-27 21:54:57 +0900

    Implement the CLI skeleton.

---
+++ bin/rdf/src/commands/format/list.rs
@@ -0,0 +1,8 @@
+// This is free and unencumbered software released into the public domain.
+
+use crate::exit::ExitCode;
+
+#[allow(dead_code)]
+pub fn list() -> Result<(), ExitCode> {
+    unimplemented!("`rdf format list` is not implemented yet") // TODO
+}
--- bin/rdf/src/main.rs
+++ bin/rdf/src/main.rs
@@ -1,3 +1,74 @@
 // This is free and unencumbered software released into the public domain.
 
-pub fn main() {}
+#![deny(unsafe_code)]
+#![allow(unused)]
+
+mod commands;
+use commands::*;
+
+mod exit;
+
+use crate::exit::ExitCode;
+use clientele::{
+    crates::clap::{Parser, Subcommand},
+    StandardOptions,
+};
+
+/// RDF.rs Command-Line Interface (CLI)
+#[derive(Debug, Parser)]
+#[command(name = "RDF.rs")]
+#[command(arg_required_else_help = true)]
+struct Options {
+    #[clap(flatten)]
+    flags: StandardOptions,
+
+    #[command(subcommand)]
+    command: Option<Command>,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    /// Query the registry of RDF formats
+    Format {
+        #[command(subcommand)]
+        command: FormatCommand,
+    },
+}
+
+#[derive(Debug, Subcommand)]
+enum FormatCommand {
+    /// List known RDF formats
+    List {},
+}
+
+pub fn main() -> Result<(), ExitCode> {
+    // Load environment variables from `.env`:
+    clientele::dotenv().ok();
+
+    // Expand wildcards and @argfiles:
+    let args = clientele::args_os()?;
+
+    // Parse command-line options:
+    let options = Options::parse_from(args);
+
+    if options.flags.version {
+        println!("RDF.rs {}", env!("CARGO_PKG_VERSION"));
+        return Ok(());
+    }
+
+    if options.flags.license {
+        println!("This is free and unencumbered software released into the public domain.");
+        return Ok(());
+    }
+
+    // Configure verbose/debug output:
+    if options.flags.verbose > 0 || options.flags.debug {
+        // TODO: configure tracing
+    }
+
+    match options.command.unwrap() {
+        Command::Format { command } => match command {
+            FormatCommand::List {} => format::list::list(),
+        },
+    }
+}
