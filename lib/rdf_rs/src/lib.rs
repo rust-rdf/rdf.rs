6dd8ff141ca39dbd3757e92da212776558d1c35d
Arto Bendiken <arto@bendiken.net>
2024-12-05 01:04:38 +0400

    Create a top-level metacrate.

---
+++ lib/rdf_rs/src/lib.rs
@@ -0,0 +1,13 @@
+// This is free and unencumbered software released into the public domain.
+
+//! ```rust
+//! # use rdf_rs::*;
+//! ```
+
+pub use rdf_derive as derive;
+pub use rdf_format as format;
+pub use rdf_model as model;
+pub use rdf_query as query;
+pub use rdf_reader as reader;
+pub use rdf_vocab as vocab;
+pub use rdf_writer as writer;
