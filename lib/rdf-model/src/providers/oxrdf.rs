05d38200b35e48e69c7fd6e4fe1488f8034389d7
Arto Bendiken <arto@bendiken.net>
2025-01-18 19:24:32 +0100

    Define a feature guard for RDF-star support.

--- lib/rdf-model/src/providers/oxrdf.rs
+++ lib/rdf-model/src/providers/oxrdf.rs
@@ -58,6 +58,8 @@ impl Term for OxrdfSubject {
         match &self.inner {
             Subject::NamedNode(_) => TermKind::Iri,
             Subject::BlankNode(_) => TermKind::BNode,
+            #[cfg(feature = "rdf-star")]
+            Subject::Triple(_) => todo!("RDF-star support not implemented yet"), // TODO
         }
     }
 
@@ -66,6 +68,8 @@ impl Term for OxrdfSubject {
         match &self.inner {
             Subject::NamedNode(node) => Cow::Borrowed(node.as_str()),
             Subject::BlankNode(node) => Cow::Borrowed(node.as_str()),
+            #[cfg(feature = "rdf-star")]
+            Subject::Triple(_) => todo!("RDF-star support not implemented yet"), // TODO
         }
     }
 }
@@ -95,6 +99,8 @@ impl Term for OxrdfTerm {
             Term::NamedNode(_) => TermKind::Iri,
             Term::BlankNode(_) => TermKind::BNode,
             Term::Literal(_) => TermKind::Literal,
+            #[cfg(feature = "rdf-star")]
+            Term::Triple(_) => todo!("RDF-star support not implemented yet"), // TODO
         }
     }
 
@@ -104,6 +110,8 @@ impl Term for OxrdfTerm {
             Term::NamedNode(node) => Cow::Borrowed(node.as_str()),
             Term::BlankNode(node) => Cow::Borrowed(node.as_str()),
             Term::Literal(lit) => Cow::Borrowed(lit.value()), // TODO
+            #[cfg(feature = "rdf-star")]
+            Term::Triple(_) => todo!("RDF-star support not implemented yet"), // TODO
         }
     }
 }
