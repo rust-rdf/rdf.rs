5ad2c9377b49419c12701ba00355cbfe4cd0596b
Samuel Sarle <samuel@sarle.com>
2025-01-18 15:14:18 +0200

    Move `BorshSerialize` implementation to `BorshTerm` from `HeapTerm`

--- lib/rdf-model/src/alloc/heap_term.rs
+++ lib/rdf-model/src/alloc/heap_term.rs
@@ -10,6 +10,10 @@ use alloc::{
 };
 
 #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[cfg_attr(
+    feature = "borsh",
+    derive(borsh::BorshSerialize, borsh::BorshDeserialize)
+)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub enum HeapTerm {
     Iri(String),
@@ -88,66 +92,5 @@ impl From<&str> for HeapTerm {
 impl From<String> for HeapTerm {
     fn from(value: String) -> Self {
         Self::literal(&value)
-    }
-}
-
-#[cfg(feature = "borsh")]
-impl borsh::BorshSerialize for HeapTerm {
-    fn serialize<W: borsh::io::Write>(&self, writer: &mut W) -> borsh::io::Result<()> {
-        match self {
-            HeapTerm::Iri(value) => {
-                0x01u8.serialize(writer)?;
-                value.serialize(writer)
-            }
-            HeapTerm::BNode(value) => {
-                0x02u8.serialize(writer)?;
-                value.serialize(writer)
-            }
-            HeapTerm::Literal(value) => {
-                0x03u8.serialize(writer)?;
-                value.serialize(writer)
-            }
-            HeapTerm::LiteralWithDatatype(value, datatype) => {
-                0x04u8.serialize(writer)?;
-                value.serialize(writer)?;
-                datatype.serialize(writer)
-            }
-            HeapTerm::LiteralWithLanguage(value, language) => {
-                0x05u8.serialize(writer)?;
-                value.serialize(writer)?;
-                language.serialize(writer)
-            }
-        }
-    }
-}
-
-#[cfg(feature = "borsh")]
-impl borsh::BorshDeserialize for HeapTerm {
-    fn deserialize_reader<R: borsh::io::Read>(reader: &mut R) -> borsh::io::Result<Self> {
-        Ok(match u8::deserialize_reader(reader)? {
-            0x01 => {
-                let value = String::deserialize_reader(reader)?;
-                HeapTerm::Iri(value)
-            }
-            0x02 => {
-                let value = String::deserialize_reader(reader)?;
-                HeapTerm::BNode(value)
-            }
-            0x03 => {
-                let value = String::deserialize_reader(reader)?;
-                HeapTerm::Literal(value)
-            }
-            0x04 => {
-                let value = String::deserialize_reader(reader)?;
-                let datatype = String::deserialize_reader(reader)?;
-                HeapTerm::LiteralWithDatatype(value, datatype)
-            }
-            0x05 => {
-                let value = String::deserialize_reader(reader)?;
-                let language = String::deserialize_reader(reader)?;
-                HeapTerm::LiteralWithLanguage(value, language)
-            }
-            _ => return Err(borsh::io::ErrorKind::InvalidData.into()),
-        })
     }
 }
