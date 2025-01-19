ce1ecfe8c2daa9d50c303ead62395601fd90e336
Ángel Iglesias Préstamo <angel.iglesias.prestamo@gmail.com>
2025-01-19 14:05:57 +0100

    implement statement for pattern

--- lib/rdf-query/src/pattern.rs
+++ lib/rdf-query/src/pattern.rs
@@ -1,4 +1,20 @@
+use rdf_model::Statement;
+
 pub struct Pattern {}
+
+impl Statement for Pattern {
+    fn subject(&self) -> &dyn rdf_model::Term {
+        todo!()
+    }
+
+    fn predicate(&self) -> &dyn rdf_model::Term {
+        todo!()
+    }
+
+    fn object(&self) -> &dyn rdf_model::Term {
+        todo!()
+    }
+}
 
 impl core::fmt::Debug for Pattern {
     fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
