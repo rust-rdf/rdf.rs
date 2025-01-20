e604f8957e01b4def50257e863ba91bd676f5603
Ángel Iglesias Préstamo <angel.iglesias.prestamo@gmail.com>
2025-01-20 13:18:30 +0100

    definition of the solution of the query and the iterator over them

--- lib/rdf-query/src/solution.rs
+++ lib/rdf-query/src/solution.rs
@@ -1,4 +1,37 @@
-pub struct Solution {}
+extern crate alloc;
+
+use alloc::collections::BTreeMap;
+use rdf_model::{HeapTerm, Term};
+
+use crate::variable::GenericVariable;
+
+pub type Solution = GenericSolution<HeapTerm>;
+
+pub struct GenericSolution<T: Term> {
+    bindings: BTreeMap<GenericVariable<T>, T>,
+}
+
+impl<T: Term + Ord> GenericSolution<T> {
+    pub fn new(bindings: BTreeMap<GenericVariable<T>, T>) -> Self {
+        Self { bindings }
+    }
+
+    pub fn binding(&self, var: &GenericVariable<T>) -> Option<&T> {
+        self.bindings.get(var)
+    }
+
+    pub fn each_binding(&self) -> impl Iterator<Item = (&GenericVariable<T>, &T)> {
+        self.bindings.iter()
+    }
+
+    pub fn each_name(&self) -> impl Iterator<Item = &GenericVariable<T>> {
+        self.bindings.keys()
+    }
+
+    pub fn each_value(&self) -> impl Iterator<Item = &T> {
+        self.bindings.values()
+    }
+}
 
 impl core::fmt::Debug for Solution {
     fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
--- lib/rdf-query/src/solutions.rs
+++ lib/rdf-query/src/solutions.rs
@@ -1,6 +1,31 @@
-pub struct Solutions {}
+extern crate alloc;
 
-impl core::fmt::Debug for Solutions {
+use alloc::boxed::Box;
+use rdf_model::Term;
+
+use crate::solution::GenericSolution;
+
+pub struct GenericSolutions<T: Term> {
+    iter: Box<dyn Iterator<Item = GenericSolution<T>>>,
+}
+
+impl<T: Term> GenericSolutions<T> {
+    pub fn new(iter: impl Iterator<Item = GenericSolution<T>> + 'static) -> Self {
+        Self {
+            iter: Box::new(iter),
+        }
+    }
+}
+
+impl<T: Term> Iterator for GenericSolutions<T> {
+    type Item = GenericSolution<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iter.next()
+    }
+}
+
+impl<T: Term> core::fmt::Debug for GenericSolutions<T> {
     fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
         f.debug_struct("Solutions").finish()
     }
