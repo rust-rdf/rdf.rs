6ff72bf6f0028ac72c5aa79bda34c82a3fbb6c83
Ángel Iglesias Préstamo <angel.iglesias.prestamo@gmail.com>
2025-01-20 13:16:54 +0100

    definition of variables to be used in triple patterns

--- lib/rdf-query/src/variable.rs
+++ lib/rdf-query/src/variable.rs
@@ -1,7 +1,30 @@
-pub struct Variable {}
+extern crate alloc;
 
-impl core::fmt::Debug for Variable {
+use alloc::string::String;
+use rdf_model::{HeapTerm, Term};
+
+pub type Variable = GenericVariable<HeapTerm>;
+
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+pub struct GenericVariable<T: Term> {
+    name: String,
+    value: Option<T>,
+}
+
+impl<T: Term> GenericVariable<T> {
+    pub fn new(name: impl Into<String>, value: Option<T>) -> Self {
+        Self {
+            name: name.into(),
+            value,
+        }
+    }
+}
+
+impl<T: Term + core::fmt::Debug> core::fmt::Debug for GenericVariable<T> {
     fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
-        f.debug_struct("Variable").finish()
+        f.debug_struct("Variable")
+            .field("name", &self.name)
+            .field("value", &self.value)
+            .finish()
     }
 }
