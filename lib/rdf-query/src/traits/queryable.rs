23ea2d3fe49499203cbb9f59c4affdebff6281d3
Ángel Iglesias Préstamo <angel.iglesias.prestamo@gmail.com>
2025-01-23 15:51:29 +0100

    query now depends on Enumerable

--- lib/rdf-query/src/traits/queryable.rs
+++ lib/rdf-query/src/traits/queryable.rs
@@ -1,24 +1,26 @@
 // This is free and unencumbered software released into the public domain.
 extern crate alloc;
 
-use alloc::boxed::Box;
-use rdf_model::{Enumerable, Statement};
+use rdf_model::Enumerable;
 
 use crate::{pattern::Pattern, query::Query, solutions::Solutions};
 
-pub trait Queryable: Enumerable + Clone {
+pub trait Queryable: Enumerable {
     // fn query(&self, query: GenericQuery<T>) -> GenericSolutions<T>;
 
-    fn query_execute(&self, query: Query) -> Solutions {
-        // query.execute(self)
-        todo!()
-    }
-
-    fn query_pattern(&self, pattern: &Pattern) -> impl Iterator<Item = Box<dyn Statement>>
+    // TODO: is this naming correct?
+    fn query_execute(&self, query: Query) -> Solutions
     where
         Self: Sized,
     {
-        self.clone() // TODO: check if the clone is necessary
-            .filter_map(move |statement| statement.ok().filter(|statement| pattern == statement))
+        query.execute(self)
+    }
+
+    // TODO: revisit this return type
+    fn query_pattern(&self, pattern: &Pattern) -> Self
+    where
+        Self: Sized,
+    {
+        self.grep(pattern)
     }
 }
