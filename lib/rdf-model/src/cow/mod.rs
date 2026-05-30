3e49c3e1ebd3665000da464fb35b8ad7ab6de881
Arto Bendiken <arto@bendiken.net>
2026-05-29 18:43:21 -0700

    Define `{Cow,Heap}{Triple,Quad}Set`.

--- lib/rdf-model/src/cow/mod.rs
+++ lib/rdf-model/src/cow/mod.rs
@@ -10,11 +10,19 @@ pub type CowTriple<'a> = Triple<CowTerm<'a>>;
 /// A clone-on-write triple statement pattern.
 pub type CowTriplePattern<'a> = TriplePattern<CowTerm<'a>>;
 
+/// A heap-allocated set of triple statements.
+#[cfg(feature = "alloc")]
+pub type CowTripleSet<'a> = alloc::collections::BTreeSet<CowTriple<'a>>;
+
 /// A clone-on-write quad statement.
 pub type CowQuad<'a> = Quad<CowTerm<'a>>;
 
 /// A clone-on-write quad statement pattern.
 pub type CowQuadPattern<'a> = QuadPattern<CowTerm<'a>>;
+
+/// A heap-allocated set of quad statements.
+#[cfg(feature = "alloc")]
+pub type CowQuadSet<'a> = alloc::collections::BTreeSet<CowQuad<'a>>;
 
 mod r#const;
 pub use r#const::*;
--- lib/rdf-model/src/heap/mod.rs
+++ lib/rdf-model/src/heap/mod.rs
@@ -8,11 +8,19 @@ pub type HeapTriple = Triple<HeapTerm>;
 /// A heap-allocated triple statement pattern.
 pub type HeapTriplePattern = TriplePattern<HeapTerm>;
 
+/// A heap-allocated set of triple statements.
+#[cfg(feature = "alloc")]
+pub type HeapTripleSet = alloc::collections::BTreeSet<HeapTriple>;
+
 /// A heap-allocated quad statement.
 pub type HeapQuad = Quad<HeapTerm>;
 
 /// A heap-allocated quad statement pattern.
 pub type HeapQuadPattern = QuadPattern<HeapTerm>;
+
+/// A heap-allocated set of quad statements.
+#[cfg(feature = "alloc")]
+pub type HeapQuadSet = alloc::collections::BTreeSet<HeapQuad>;
 
 mod term;
 pub use term::*;
