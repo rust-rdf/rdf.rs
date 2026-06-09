395b62e3d22025c01584c288a777c048420d91fc
Arto Bendiken <arto@bendiken.net>
2026-06-09 02:02:07 -0700

    Stub the rdf-hash and rdf-id crates.

---
+++ lib/rdf-hash/src/lib.rs
@@ -0,0 +1,10 @@
+// This is free and unencumbered software released into the public domain.
+
+//! RDF.rs is a Rust framework for working with RDF knowledge graphs.
+//!
+//! ```rust
+//! use rdf_hash::*;
+//! ```
+
+#![no_std]
+#![deny(unsafe_code)]
