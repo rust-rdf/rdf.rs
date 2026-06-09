2ed762d04ed906a3f84e71242fbd9b3aaee4c943
Arto Bendiken <arto@bendiken.net>
2026-06-09 02:56:41 -0700

    Implement `TermHash` and `TripleHash`.

--- lib/rdf-hash/src/term_hash.rs
+++ lib/rdf-hash/src/term_hash.rs
@@ -1,5 +1,79 @@
 // This is free and unencumbered software released into the public domain.
 
+use blake3::Hash;
+
+/// The length of a term hash in bytes.
 pub const TERM_HASH_LEN: usize = blake3::OUT_LEN;
 
-pub type TermHash = blake3::Hash;
+/// The all-zero bytes value of a term hash.
+pub const TERM_HASH_ZERO: [u8; TERM_HASH_LEN] = [0u8; TERM_HASH_LEN];
+
+/// A cryptographically-secure hash of a term.
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct TermHash(Hash);
+
+impl TermHash {
+    /// The all-zero term hash.
+    pub const ZERO: Self = Self::from_bytes(TERM_HASH_ZERO);
+
+    pub const fn from_bytes(input: [u8; TERM_HASH_LEN]) -> Self {
+        Self(Hash::from_bytes(input))
+    }
+
+    pub fn as_bytes(&self) -> &[u8; TERM_HASH_LEN] {
+        self.0.as_bytes()
+    }
+
+    pub fn as_slice(&self) -> &[u8] {
+        self.0.as_slice()
+    }
+}
+
+impl Default for TermHash {
+    fn default() -> Self {
+        Self::ZERO
+    }
+}
+
+impl From<[u8; TERM_HASH_LEN]> for TermHash {
+    fn from(input: [u8; TERM_HASH_LEN]) -> Self {
+        Self::from_bytes(input)
+    }
+}
+
+impl From<&[u8; TERM_HASH_LEN]> for TermHash {
+    fn from(input: &[u8; TERM_HASH_LEN]) -> Self {
+        Self::from_bytes(*input)
+    }
+}
+
+impl From<TermHash> for [u8; TERM_HASH_LEN] {
+    fn from(input: TermHash) -> Self {
+        input.0.into()
+    }
+}
+
+impl From<Hash> for TermHash {
+    fn from(input: Hash) -> Self {
+        Self(input)
+    }
+}
+
+impl From<&Hash> for TermHash {
+    fn from(input: &Hash) -> Self {
+        Self(input.clone())
+    }
+}
+
+impl From<TermHash> for Hash {
+    fn from(input: TermHash) -> Self {
+        input.0
+    }
+}
+
+impl From<&TermHash> for Hash {
+    fn from(input: &TermHash) -> Self {
+        input.0.clone()
+    }
+}
--- lib/rdf-hash/src/triple_hash.rs
+++ lib/rdf-hash/src/triple_hash.rs
@@ -1,5 +1,108 @@
 // This is free and unencumbered software released into the public domain.
 
+use crate::TermHash;
+use blake3::{Hash, Hasher};
+
+/// The length of a triple hash in bytes.
 pub const TRIPLE_HASH_LEN: usize = blake3::OUT_LEN;
 
-pub type TripleHash = blake3::Hash;
+/// The all-zero bytes of a triple hash.
+pub const TRIPLE_HASH_ZERO: [u8; TRIPLE_HASH_LEN] = [0u8; TRIPLE_HASH_LEN];
+
+/// A cryptographically-secure hash of a triple.
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct TripleHash(Hash);
+
+impl TripleHash {
+    /// The all-zero triple hash.
+    pub const ZERO: Self = Self::from_bytes(TRIPLE_HASH_ZERO);
+
+    pub const fn from_bytes(input: [u8; TRIPLE_HASH_LEN]) -> Self {
+        Self(Hash::from_bytes(input))
+    }
+
+    pub fn as_bytes(&self) -> &[u8; TRIPLE_HASH_LEN] {
+        self.0.as_bytes()
+    }
+
+    pub fn as_slice(&self) -> &[u8] {
+        self.0.as_slice()
+    }
+}
+
+impl Default for TripleHash {
+    fn default() -> Self {
+        Self::ZERO
+    }
+}
+
+impl From<[u8; TRIPLE_HASH_LEN]> for TripleHash {
+    fn from(input: [u8; TRIPLE_HASH_LEN]) -> Self {
+        Self::from_bytes(input)
+    }
+}
+
+impl From<&[u8; TRIPLE_HASH_LEN]> for TripleHash {
+    fn from(input: &[u8; TRIPLE_HASH_LEN]) -> Self {
+        Self::from_bytes(*input)
+    }
+}
+
+impl From<TripleHash> for [u8; TRIPLE_HASH_LEN] {
+    fn from(input: TripleHash) -> Self {
+        input.0.into()
+    }
+}
+
+impl From<Hash> for TripleHash {
+    fn from(input: Hash) -> Self {
+        Self(input)
+    }
+}
+
+impl From<&Hash> for TripleHash {
+    fn from(input: &Hash) -> Self {
+        Self(input.clone())
+    }
+}
+
+impl From<TripleHash> for Hash {
+    fn from(input: TripleHash) -> Self {
+        input.0
+    }
+}
+
+impl From<&TripleHash> for Hash {
+    fn from(input: &TripleHash) -> Self {
+        input.0.clone()
+    }
+}
+
+impl From<(TermHash, TermHash, TermHash)> for TripleHash {
+    fn from((s, p, o): (TermHash, TermHash, TermHash)) -> Self {
+        (&s, &p, &o).into()
+    }
+}
+
+impl From<(&TermHash, &TermHash, &TermHash)> for TripleHash {
+    fn from((s, p, o): (&TermHash, &TermHash, &TermHash)) -> Self {
+        let mut hasher = Hasher::new();
+        hasher.update(s.as_slice());
+        hasher.update(p.as_slice());
+        hasher.update(o.as_slice());
+        Self(hasher.finalize())
+    }
+}
+
+impl From<(TermHash, TermHash, TermHash, TermHash)> for TripleHash {
+    fn from((s, p, o, _): (TermHash, TermHash, TermHash, TermHash)) -> Self {
+        (&s, &p, &o).into()
+    }
+}
+
+impl From<(&TermHash, &TermHash, &TermHash, &TermHash)> for TripleHash {
+    fn from((s, p, o, _): (&TermHash, &TermHash, &TermHash, &TermHash)) -> Self {
+        (s, p, o).into()
+    }
+}
