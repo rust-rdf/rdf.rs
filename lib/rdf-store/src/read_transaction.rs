9187eb31c79a9a2022e370dcba06c993cb83a158
Arto Bendiken <arto@bendiken.net>
2026-06-18 18:04:43 -0700

    Define `ReadTransaction#is_empty()`.

--- lib/rdf-store/src/read_transaction.rs
+++ lib/rdf-store/src/read_transaction.rs
@@ -2,14 +2,20 @@
 
 use core::hash::Hash;
 use futures::stream::{self, Stream};
-use rdf_model::{Statement, StatementPattern, Term};
+use rdf_model::{QuadPattern, Statement, StatementPattern, Term};
 
 /// A read-only (R/O) transaction on a [`Store`].
 pub trait ReadTransaction {
     type Error;
     type Term: Term + Clone + PartialEq + Eq + Hash;
     type Statement: Statement<Term = Self::Term>;
-    type StatementPattern: StatementPattern<Term = Self::Term>;
+    type StatementPattern: StatementPattern<Term = Self::Term> + From<QuadPattern<Self::Term>>;
+
+    /// Returns `true` if the store is empty (contains no statements).
+    fn is_empty(&self) -> impl Future<Output = Result<bool, Self::Error>> {
+        use futures::future::TryFutureExt;
+        self.count(QuadPattern::default()).map_ok(|count| count > 0)
+    }
 
     /// Returns a stream of all context terms (graph names) in the store.
     fn contexts(&self) -> impl Stream<Item = Result<Self::Term, Self::Error>> {
